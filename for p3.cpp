a50d068e665d3fee08c9a7396b09c9ad7ec9a5eb
farhalnagaar <139056244+farhalnagaar@users.noreply.github.com>
2023-10-20 01:49:22 +0300

    Update for p3.cpp

--- for p3.cpp
+++ for p3.cpp
@@ -5,21 +5,27 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
+    
     //حل مشكله بfor
-    //for (int x= 0 ;x<=30 ;x=x+3){
-       // cout << x <<endl ;
-    //}
+    
+    for (int x= 0 ;x<=30 ;x=x+3){
+        cout << x <<endl ;
+    }
+    
+    // حل المساله ب while
+    
     int x=0;
-// حل المساله ب while
     while(x<=30){
            cout <<x<<endl;
              x=x+3;}
+    
 //حل المساله ب do-while
-    //int x=0;
-    //do{
-        //cout << x<< endl;
-       // x=x+3;
-    //}while(x<=30);
+    
+    int x=0;
+    do{
+        cout << x<< endl;
+        x=x+3;
+    }while(x<=30);
 
 
     return 0;
